988a5945e2d0380aba5f126c55d63af77359ace7
Matias Barria Lopez <mbarrial@cmslpc327.fnal.gov>
2024-10-07 13:59:37 -0500

    lib/Run3Constants.h

--- lib/CreateHistograms.h
+++ lib/CreateHistograms.h
@@ -13,7 +13,8 @@
 class CreateHistograms {
   public:
     // Methods
-    CreateHistograms(TString input, TString output, TString era, TString channel);
+    CreateHistograms(TString input, TString output, TString era,
+                     TString channel);
     virtual ~CreateHistograms();
     double eventWeight();
     void fillChain();
@@ -27,24 +28,33 @@ class CreateHistograms {
     // Members
     TChain *tuple;
     float diMuon_mass, diMuon_pt, diMuon_phi, diMuon_eta, gen_weight,
-        pileup_weight;
+        pileup_weight, pileup;
+    // int mu1_index, mu2_index;
+    // std::vector<float> t_Mu_pt;
     long double gen_weight_sum;
     double scale_factor; // luminosity * cross_section / gen_weight_sum
     const float luminosity;
     const float cross_section;
+    // const TString era;
+    // const TString channel;
     TH1F diMuon_mass_hist, diMuon_pt_hist, diMuon_eta_hist, diMuon_phi_hist;
+    TH1F diMuon_mass_hist_full_range, diMuon_pileup_hist,
+        diMuon_pileup_hist_weighted, diMuon_pileup_hist_gen_weighted,
+        diMuon_pileup_hist_pugen_weighted;
     TString input_name;
     TString output_name;
+    TString output_directory;
     TFile *output_file;
 };
 
 CreateHistograms::CreateHistograms(TString input, TString output, TString era,
-                             TString channel)
+                                   TString channel)
     : luminosity(LUMINOSITY.at(era)), cross_section(CROSS_SECTION.at(channel)) {
 
     tuple = new TChain("tree");
     input_name = input;
-    output_name = output;
+    output_directory = output;
+    output_name = channel + "_" + era + "_histograms.root";
     std::cout << "Luminosity: " << luminosity
               << ", cross section: " << cross_section << std::endl;
     fillChain();
@@ -67,6 +77,15 @@ void CreateHistograms::defineHistograms() {
     diMuon_pt_hist = TH1F("diMuon_pt", "diMuon_pt", 250, 0, 250);
     diMuon_phi_hist = TH1F("diMuon_phi", "diMuon_phi", 100, -3.1415, 3.1415);
     diMuon_eta_hist = TH1F("diMuon_eta", "diMuon_eta", 200, -10, 10);
+    diMuon_mass_hist_full_range =
+        TH1F("diMuon_mass_full_range", "diMuon_mass_full_range", 200, 50, 150);
+    diMuon_pileup_hist = TH1F("diMuon_pileup", "diMuon_pileup", 120, 0, 120);
+    diMuon_pileup_hist_gen_weighted =
+        TH1F("diMuon_pileup_hist_gen_weighted", "diMuon_pileup_gen_weighted",
+             120, 0, 120);
+    diMuon_pileup_hist_pugen_weighted =
+        TH1F("diMuon_pileup_hist_pugen_weighted",
+             "diMuon_pileup_pugen_weighted", 120, 0, 120);
 }
 
 void CreateHistograms::setBranchesAddresses() {
@@ -77,6 +96,7 @@ void CreateHistograms::setBranchesAddresses() {
     tuple->SetBranchAddress("t_diMuon_eta", &diMuon_eta);
     tuple->SetBranchAddress("t_genWeight", &gen_weight);
     tuple->SetBranchAddress("t_puWeight", &pileup_weight);
+    // tuple->SetBranchAddress("t_pileup", &pileup);
     std::cout << "Branches addressed setted" << std::endl;
 }
 
@@ -92,6 +112,7 @@ void CreateHistograms::fillHistogramsData() {
         if (diMuon_mass > 120 && diMuon_mass < 130)
             continue;
         diMuon_mass_hist.Fill(diMuon_mass);
+        diMuon_mass_hist_full_range.Fill(diMuon_mass);
     }
     std::cout << "Histograms filled" << std::endl;
 }
@@ -108,6 +129,12 @@ void CreateHistograms::fillHistogramsSimulation() {
         diMuon_mass_hist.Fill(diMuon_mass, weight);
         diMuon_phi_hist.Fill(diMuon_phi, weight);
         diMuon_eta_hist.Fill(diMuon_eta, weight);
+        diMuon_mass_hist_full_range.Fill(diMuon_mass, weight);
+        // diMuon_pileup_hist.Fill(pileup);
+        // diMuon_pileup_hist_pugen_weighted.Fill(pileup, weight);
+        // if (pileup_weight != 0) {
+        // diMuon_pileup_hist_gen_weighted.Fill(pileup, weight / pileup_weight);
+        //}
     }
     std::cout << "Histograms filled" << std::endl;
 }
@@ -121,6 +148,8 @@ void CreateHistograms::getGenWeightSum() {
     }
     TH1F *gen_weight_sum_histogram =
         (TH1F *)tuple_file->Get("h_sumOfgenWeight");
+    // TH1F *gen_weight_sum_histogram =
+    //(TH1F *)tuple_file->Get("h_sumOfgenpuWeight");
 
     if (!gen_weight_sum_histogram) {
         std::cerr << "Sum gen weight histogram WAS NOT found" << std::endl;
@@ -139,18 +168,28 @@ void CreateHistograms::getScaleFactor() {
 }
 
 double CreateHistograms::eventWeight() {
-    return gen_weight * pileup_weight * scale_factor;
-    //  return gen_weight * scale_factor;
+    // return gen_weight * pileup_weight * scale_factor;
+    //  if(pileup_weight > 100000){
+    //  std::cout << "pileup: " <<  pileup<< std::endl;
+    //  std::cout << "pileup_weight: " <<  pileup_weight << std::endl;
+    //  std::cout << "gen_weight: " <<  gen_weight << std::endl;
+    //  std::cout << "------------------------" << std::endl;
+    ////}
+    return gen_weight * scale_factor;
 }
 
 void CreateHistograms::saveHistograms() {
 
-    output_file = new TFile("./root_io/" + output_name, "RECREATE");
+    output_file = new TFile(output_directory + output_name, "RECREATE");
     output_file->cd();
     diMuon_mass_hist.Write();
     diMuon_pt_hist.Write();
     diMuon_phi_hist.Write();
     diMuon_eta_hist.Write();
+    diMuon_mass_hist_full_range.Write();
+    diMuon_pileup_hist.Write();
+    diMuon_pileup_hist_pugen_weighted.Write();
+    diMuon_pileup_hist_gen_weighted.Write();
     output_file->Close();
     std::cout << "Histograms saved" << std::endl;
 }
