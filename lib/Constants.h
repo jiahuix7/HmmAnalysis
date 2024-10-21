b8c95e90d889a23360e6bc3329777ebbc025bb73
Matias Barria Lopez <mbarrial@cmslpc306.fnal.gov>
2024-10-21 14:16:43 -0500

    Skin tuple for xgboot training

---
+++ lib/Constants.h
@@ -0,0 +1,10 @@
+#ifndef LIB_CONTANTS_H
+#define LIB_CONTANTS_H
+
+constexpr float MUON_MASS = 0.1056583745;
+constexpr float PROTON_MASS = 0.938272;
+constexpr float BEAM_ENERGY_GEV = 13600.0;
+
+// float proton_mass = ;
+
+#endif // LIB_CONTANTS_H
