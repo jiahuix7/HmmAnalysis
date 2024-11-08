ad7fda2263c03d93c25e466ea2d7c18881c659e6
Claudio San Martin <claudio.sanmartinval@gmail.com>
2024-11-08 14:38:31 -0600

    Save merged trees in a subfolder

--- root_io/skim_VBF/hadd_skim.sh
+++ root_io/skim_VBF/hadd_skim.sh
@@ -1,37 +1,61 @@
-##############  2022  ##############
-hadd background_2022_skim.root WWto2L2Nu_2022_skim.root WZto2L2Q_2022_skim.root WZto3LNu_2022_skim.root ZZto2L2Q_2022_skim.root ZZto2L2Nu_2022_skim.root ZZto4L_2022_skim.root TTto2L2Nu_2022_skim.root TTtoLNu2Q_2022_skim.root DY50to120_2022_skim.root DY120to200_2022_skim.root EWK_2L2J_2022_skim.root
-hadd background_2022_skimNO50120.root WWto2L2Nu_2022_skim.root WZto2L2Q_2022_skim.root WZto3LNu_2022_skim.root ZZto2L2Q_2022_skim.root ZZto2L2Nu_2022_skim.root ZZto4L_2022_skim.root TTto2L2Nu_2022_skim.root TTtoLNu2Q_2022_skim.root  DY120to200_2022_skim.root EWK_2L2J_2022_skim.root
 
-hadd signal_2022_skim.root ggH_2022_skim.root  VBF_2022_skim.root ttH_2022_skim.root
+mkdir -p merged
+##############  2022  ##############
+hadd merged/background_2022_skim.root DY50to120_2022_skim.root DY120to200_2022_skim.root EWK_2L2J_2022_skim.root TTto2L2Nu_2022_skim.root TTtoLNu2Q_2022_skim.root TWminusto2L2Nu_2022_skim.root WZto2L2Q_2022_skim.root WZto3LNu_2022_skim.root WZtoLNu2Q_2022_skim.root ZZto2L2Nu_2022_skim.root ZZto2L2Q_2022_skim.root ZZto4L_2022_skim.root WWto2L2Nu_2022_skim.root WWtoLNu2Q_2022_skim.root
+hadd merged/background_2022_skim2L.root DY50to120_2022_skim.root DY120to200_2022_skim.root EWK_2L2J_2022_skim.root TTto2L2Nu_2022_skim.root TTtoLNu2Q_2022_skim.root TWminusto2L2Nu_2022_skim.root WZto2L2Q_2022_skim.root WZto3LNu_2022_skim.root ZZto2L2Nu_2022_skim.root ZZto2L2Q_2022_skim.root ZZto4L_2022_skim.root WWto2L2Nu_2022_skim.root
+hadd merged/background_2022_skimNoDY50.root DY120to200_2022_skim.root EWK_2L2J_2022_skim.root TTto2L2Nu_2022_skim.root TTtoLNu2Q_2022_skim.root TWminusto2L2Nu_2022_skim.root WZto2L2Q_2022_skim.root WZto3LNu_2022_skim.root ZZto2L2Nu_2022_skim.root ZZto2L2Q_2022_skim.root ZZto4L_2022_skim.root WWto2L2Nu_2022_skim.root
+
+hadd merged/signal_2022_skim.root ggH_2022_skim.root  VBF_2022_skim.root ttH_2022_skim.root
+hadd merged/signal_2022_skimNottH.root ggH_2022_skim.root  VBF_2022_skim.root
+hadd merged/signal_2022_skimVBF.root VBF_2022_skim.root
 
 ##############  2022EE  ##############
-hadd background_2022EE_skim.root TTto2L2Nu_2022EE_skim.root TTtoLNu2Q_2022EE_skim.root  WWto2L2Nu_2022EE_skim.root WZto2L2Q_2022EE_skim.root WZto3LNu_2022EE_skim.root ZZto2L2Q_2022EE_skim.root ZZto2L2Nu_2022EE_skim.root ZZto4L_2022EE_skim.root DY50to120_2022EE_skim.root DY120to200_2022EE_skim.root EWK_2L2J_2022EE_skim.root
-hadd background_2022EE_skimNO50120.root TTto2L2Nu_2022EE_skim.root TTtoLNu2Q_2022EE_skim.root  WWto2L2Nu_2022EE_skim.root WZto2L2Q_2022EE_skim.root WZto3LNu_2022EE_skim.root ZZto2L2Q_2022EE_skim.root ZZto2L2Nu_2022EE_skim.root ZZto4L_2022EE_skim.root  DY120to200_2022EE_skim.root EWK_2L2J_2022EE_skim.root
+hadd merged/background_2022EE_skim.root DY50to120_2022EE_skim.root DY120to200_2022EE_skim.root EWK_2L2J_2022EE_skim.root TTto2L2Nu_2022EE_skim.root TTtoLNu2Q_2022EE_skim.root WZto2L2Q_2022EE_skim.root WZto3LNu_2022EE_skim.root WZtoLNu2Q_2022EE_skim.root ZZto2L2Nu_2022EE_skim.root ZZto2L2Q_2022EE_skim.root ZZto4L_2022EE_skim.root WWto2L2Nu_2022EE_skim.root WWtoLNu2Q_2022EE_skim.root
+hadd merged/background_2022EE_skim2L.root DY50to120_2022EE_skim.root DY120to200_2022EE_skim.root EWK_2L2J_2022EE_skim.root TTto2L2Nu_2022EE_skim.root TTtoLNu2Q_2022EE_skim.root WZto2L2Q_2022EE_skim.root WZto3LNu_2022EE_skim.root ZZto2L2Nu_2022EE_skim.root ZZto2L2Q_2022EE_skim.root ZZto4L_2022EE_skim.root WWto2L2Nu_2022EE_skim.root
+hadd merged/background_2022EE_skimNoDY50.root DY120to200_2022EE_skim.root EWK_2L2J_2022EE_skim.root TTto2L2Nu_2022EE_skim.root TTtoLNu2Q_2022EE_skim.root WZto2L2Q_2022EE_skim.root WZto3LNu_2022EE_skim.root ZZto2L2Nu_2022EE_skim.root ZZto2L2Q_2022EE_skim.root ZZto4L_2022EE_skim.root WWto2L2Nu_2022EE_skim.root
 
-hadd signal_2022EE_skim.root ggH_2022EE_skim.root ttH_2022EE_skim.root VBF_2022EE_skim.root
+hadd merged/signal_2022EE_skim.root ggH_2022EE_skim.root VBF_2022EE_skim.root ttH_2022EE_skim.root
+hadd merged/signal_2022EE_skimNottH.root ggH_2022EE_skim.root VBF_2022EE_skim.root
+hadd merged/signal_2022EE_skimVBF.root VBF_2022EE_skim.root
 
 ##############  2022Combined  ##############
-hadd background_2022Combined_skim.root background_2022_skim.root background_2022EE_skim.root
+hadd merged/background_2022Combined_skim.root merged/background_2022_skim.root merged/background_2022EE_skim.root
+hadd merged/background_2022Combined_skimNoDY50.root merged/background_2022_skimNoDY50.root merged/background_2022EE_skimNoDY50.root
 
-hadd signal_2022Combined_skim.root signal_2022_skim.root signal_2022EE_skim.root
-
-# hadd ggH_2022Combined_skim.root ggH_2022_skim.root ggH_2022EE_skim.root
+hadd merged/signal_2022Combined_skim.root merged/signal_2022_skim.root merged/signal_2022EE_skim.root
+hadd merged/signal_2022Combined_skimNottH.root merged/signal_2022_skimNottH.root merged/signal_2022EE_skimNottH.root
+hadd merged/signal_2022Combined_skimVBF.root merged/signal_2022_skimVBF.root merged/signal_2022EE_skimVBF.root
 
 ##############  2023  ##############
-hadd background_2023_skim.root WWto2L2Nu_2023_skim.root WZto2L2Q_2023_skim.root WZto3LNu_2023_skim.root ZZto2L2Q_2023_skim.root ZZto2L2Nu_2023_skim.root ZZto4L_2023_skim.root TTto2L2Nu_2023_skim.root TTtoLNu2Q_2023_skim.root DY50to120_2023_skim.root DY120to200_2023_skim.root EWK_2L2J_2023_skim.root
-hadd background_2023_skimNO50120.root WWto2L2Nu_2023_skim.root WZto2L2Q_2023_skim.root WZto3LNu_2023_skim.root ZZto2L2Q_2023_skim.root ZZto2L2Nu_2023_skim.root ZZto4L_2023_skim.root TTto2L2Nu_2023_skim.root TTtoLNu2Q_2023_skim.root  DY120to200_2023_skim.root EWK_2L2J_2023_skim.root
+hadd merged/background_2023_skim.root DY50to120_2023_skim.root DY120to200_2023_skim.root EWK_2L2J_2023_skim.root TTto2L2Nu_2023_skim.root TTtoLNu2Q_2023_skim.root WZto2L2Q_2023_skim.root WZto3LNu_2023_skim.root WZtoLNu2Q_2023_skim.root ZZto2L2Nu_2023_skim.root ZZto2L2Q_2023_skim.root ZZto4L_2023_skim.root WWto2L2Nu_2023_skim.root WWtoLNu2Q_2023_skim.root
+hadd merged/background_2023_skim2L.root DY50to120_2023_skim.root DY120to200_2023_skim.root EWK_2L2J_2023_skim.root TTto2L2Nu_2023_skim.root TTtoLNu2Q_2023_skim.root WZto2L2Q_2023_skim.root WZto3LNu_2023_skim.root ZZto2L2Nu_2023_skim.root ZZto2L2Q_2023_skim.root ZZto4L_2023_skim.root WWto2L2Nu_2023_skim.root
+hadd merged/background_2023_skimNoDY50.root DY120to200_2023_skim.root EWK_2L2J_2023_skim.root TTto2L2Nu_2023_skim.root TTtoLNu2Q_2023_skim.root WZto2L2Q_2023_skim.root WZto3LNu_2023_skim.root ZZto2L2Nu_2023_skim.root ZZto2L2Q_2023_skim.root ZZto4L_2023_skim.root WWto2L2Nu_2023_skim.root
 
-hadd signal_2023_skim.root ggH_2023_skim.root  VBF_2023_skim.root ttH_2023_skim.root
+hadd merged/signal_2023_skim.root ggH_2023_skim.root VBF_2023_skim.root ttH_2023_skim.root
+hadd merged/signal_2023_skimNottH.root ggH_2023_skim.root VBF_2023_skim.root
+hadd merged/signal_2023_skimVBF.root VBF_2023_skim.root
 
 ##############  2023BPix  ##############
-hadd background_2023BPix_skim.root TTto2L2Nu_2023BPix_skim.root TTtoLNu2Q_2023BPix_skim.root  WWto2L2Nu_2023BPix_skim.root WZto2L2Q_2023BPix_skim.root WZto3LNu_2023BPix_skim.root ZZto2L2Q_2023BPix_skim.root ZZto2L2Nu_2023BPix_skim.root ZZto4L_2023BPix_skim.root DY50to120_2023BPix_skim.root DY120to200_2023BPix_skim.root EWK_2L2J_2023BPix_skim.root
-hadd background_2023BPix_skimNO50120.root TTto2L2Nu_2023BPix_skim.root TTtoLNu2Q_2023BPix_skim.root  WWto2L2Nu_2023BPix_skim.root WZto2L2Q_2023BPix_skim.root WZto3LNu_2023BPix_skim.root ZZto2L2Q_2023BPix_skim.root ZZto2L2Nu_2023BPix_skim.root ZZto4L_2023BPix_skim.root  DY120to200_2023BPix_skim.root EWK_2L2J_2023BPix_skim.root
+hadd merged/background_2023BPix_skim.root DY50to120_2023BPix_skim.root DY120to200_2023BPix_skim.root EWK_2L2J_2023BPix_skim.root TTto2L2Nu_2023BPix_skim.root TTtoLNu2Q_2023BPix_skim.root WZto2L2Q_2023BPix_skim.root WZto3LNu_2023BPix_skim.root WZtoLNu2Q_2023BPix_skim.root ZZto2L2Nu_2023BPix_skim.root ZZto2L2Q_2023BPix_skim.root ZZto4L_2023BPix_skim.root WWto2L2Nu_2023BPix_skim.root WWtoLNu2Q_2023BPix_skim.root
+hadd merged/background_2023BPix_skim2L.root DY50to120_2023BPix_skim.root DY120to200_2023BPix_skim.root EWK_2L2J_2023BPix_skim.root TTto2L2Nu_2023BPix_skim.root TTtoLNu2Q_2023BPix_skim.root WZto2L2Q_2023BPix_skim.root WZto3LNu_2023BPix_skim.root ZZto2L2Nu_2023BPix_skim.root ZZto2L2Q_2023BPix_skim.root ZZto4L_2023BPix_skim.root WWto2L2Nu_2023BPix_skim.root
+hadd merged/background_2023BPix_skimNoDY50.root DY120to200_2023BPix_skim.root EWK_2L2J_2023BPix_skim.root TTto2L2Nu_2023BPix_skim.root TTtoLNu2Q_2023BPix_skim.root WZto2L2Q_2023BPix_skim.root WZto3LNu_2023BPix_skim.root ZZto2L2Nu_2023BPix_skim.root ZZto2L2Q_2023BPix_skim.root ZZto4L_2023BPix_skim.root WWto2L2Nu_2023BPix_skim.root
 
-hadd signal_2023BPix_skim.root ggH_2023BPix_skim.root ttH_2023BPix_skim.root VBF_2023BPix_skim.root
+hadd merged/signal_2023BPix_skim.root ggH_2023BPix_skim.root VBF_2023BPix_skim.root ttH_2023BPix_skim.root
+hadd merged/signal_2023BPix_skimNottH.root ggH_2023BPix_skim.root VBF_2023BPix_skim.root
+hadd merged/signal_2023BPix_skimVBF.root VBF_2023BPix_skim.root
 
 ##############  2023Combined  ##############
-hadd background_2023Combined_skim.root background_2023_skim.root background_2023BPix_skim.root
+hadd merged/background_2023Combined_skim.root merged/background_2023_skim.root merged/background_2023BPix_skim.root
+hadd merged/background_2023Combined_skimNoDY50.root merged/background_2023_skimNoDY50.root merged/background_2023BPix_skimNoDY50.root
 
-hadd signal_2023Combined_skim.root signal_2023_skim.root signal_2023BPix_skim.root
+hadd merged/signal_2023Combined_skim.root merged/signal_2023_skim.root merged/signal_2023BPix_skim.root
+hadd merged/signal_2023Combined_skimNottH.root merged/signal_2023_skimNottH.root merged/signal_2023BPix_skimNottH.root
+hadd merged/signal_2023Combined_skimVBF.root merged/signal_2023_skimVBF.root merged/signal_2023BPix_skimVBF.root
 
-# hadd ggH_2023Combined_skim.root ggH_2023_skim.root ggH_2023BPix_skim.root
+##############  Combined  ##############
+hadd merged/background_Combined_skim.root merged/background_2022Combined_skim.root merged/background_2023Combined_skim.root
+hadd merged/background_Combined_skimNoDY50.root merged/background_2022Combined_skimNoDY50.root merged/background_2023Combined_skimNoDY50.root
+
+hadd merged/signal_Combined_skim.root merged/signal_2022Combined_skim.root merged/signal_2023Combined_skim.root
+hadd merged/signal_Combined_skimNottH.root merged/signal_2022Combined_skimNottH.root merged/signal_2023Combined_skimNottH.root
+hadd merged/signal_Combined_skimVBF.root merged/signal_2022Combined_skimVBF.root merged/signal_2023Combined_skimVBF.root
