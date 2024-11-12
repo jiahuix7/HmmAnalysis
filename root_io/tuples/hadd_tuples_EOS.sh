da1348285cd17eadf65b28e8cf19a2a23f68fd26
Claudio San Martin <claudio.sanmartinval@gmail.com>
2024-11-12 15:53:47 -0600

    Hadd tuples to draw vs_data and update macro to show events in a category

---
+++ root_io/tuples/hadd_tuples_EOS.sh
@@ -0,0 +1,51 @@
+
+EOS_PATH="/eos/uscms/store/user/csanmart/analyzer_HiggsMuMu/tuples/"
+##############  2022  ##############
+# hadd Data_2022_tuples.root ${EOS_PATH}DoubleMuon_2022C_2022_tuples.root ${EOS_PATH}Muon_2022C_2022_tuples.root ${EOS_PATH}Muon_2022D_2022_tuples.root
+
+hadd DY_2022_tuples.root ${EOS_PATH}DY50to120_2022_tuples.root ${EOS_PATH}DY120to200_2022_tuples.root
+hadd EWK_2022_tuples.root ${EOS_PATH}EWK_2L2J_2022_tuples.root
+hadd TT_2022_tuples.root ${EOS_PATH}TTto2L2Nu_2022_tuples.root ${EOS_PATH}TTtoLNu2Q_2022_tuples.root
+hadd DiBoson_2022_tuples.root ${EOS_PATH}WZto2L2Q_2022_tuples.root ${EOS_PATH}WZto3LNu_2022_tuples.root ${EOS_PATH}ZZto2L2Nu_2022_tuples.root ${EOS_PATH}ZZto2L2Q_2022_tuples.root ${EOS_PATH}ZZto4L_2022_tuples.root ${EOS_PATH}WWto2L2Nu_2022_tuples.root
+
+hadd ggH_2022_tuples.root ${EOS_PATH}ggH_2022_tuples.root
+hadd VBF_2022_tuples.root ${EOS_PATH}VBF_2022_tuples.root
+hadd ttH_2022_tuples.root ${EOS_PATH}ttH_2022_tuples.root
+
+##############  2022EE  ##############
+
+hadd Data_2022EE_tuples.root ${EOS_PATH}Muon_2022E_2022EE_tuples.root ${EOS_PATH}Muon_2022F_2022EE_tuples.root ${EOS_PATH}Muon_2022G_2022EE_tuples.root
+
+hadd DY_2022EE_tuples.root ${EOS_PATH}DY50to120_2022EE_tuples.root ${EOS_PATH}DY120to200_2022EE_tuples.root
+hadd EWK_2022EE_tuples.root ${EOS_PATH}EWK_2L2J_2022EE_tuples.root
+hadd TT_2022EE_tuples.root ${EOS_PATH}TTto2L2Nu_2022EE_tuples.root ${EOS_PATH}TTtoLNu2Q_2022EE_tuples.root
+hadd DiBoson_2022EE_tuples.root ${EOS_PATH}WZto2L2Q_2022EE_tuples.root ${EOS_PATH}WZto3LNu_2022EE_tuples.root ${EOS_PATH}ZZto2L2Nu_2022EE_tuples.root ${EOS_PATH}ZZto2L2Q_2022EE_tuples.root ${EOS_PATH}ZZto4L_2022EE_tuples.root ${EOS_PATH}WWto2L2Nu_2022EE_tuples.root
+
+hadd ggH_2022EE_tuples.root ${EOS_PATH}ggH_2022EE_tuples.root
+hadd VBF_2022EE_tuples.root ${EOS_PATH}VBF_2022EE_tuples.root
+hadd ttH_2022EE_tuples.root ${EOS_PATH}ttH_2022EE_tuples.root
+
+##############  2023  ##############
+
+hadd Data_2023_tuples.root ${EOS_PATH}Muon0_2023C_v1_2023_tuples.root ${EOS_PATH}Muon0_2023C_v2_2023_tuples.root ${EOS_PATH}Muon0_2023C_v3_2023_tuples.root ${EOS_PATH}Muon0_2023C_v4_2023_tuples.root ${EOS_PATH}Muon1_2023C_v1_2023_tuples.root ${EOS_PATH}Muon1_2023C_v2_2023_tuples.root ${EOS_PATH}Muon1_2023C_v3_2023_tuples.root ${EOS_PATH}Muon1_2023C_v4_2023_tuples.root
+
+hadd DY_2023_tuples.root ${EOS_PATH}DY50to120_2023_tuples.root ${EOS_PATH}DY120to200_2023_tuples.root
+hadd EWK_2023_tuples.root ${EOS_PATH}EWK_2L2J_2023_tuples.root
+hadd TT_2023_tuples.root ${EOS_PATH}TTto2L2Nu_2023_tuples.root ${EOS_PATH}TTtoLNu2Q_2023_tuples.root
+hadd DiBoson_2023_tuples.root ${EOS_PATH}WZto2L2Q_2023_tuples.root ${EOS_PATH}WZto3LNu_2023_tuples.root ${EOS_PATH}ZZto2L2Nu_2023_tuples.root ${EOS_PATH}ZZto2L2Q_2023_tuples.root ${EOS_PATH}ZZto4L_2023_tuples.root ${EOS_PATH}WWto2L2Nu_2023_tuples.root
+
+hadd ggH_2023_tuples.root ${EOS_PATH}ggH_2023_tuples.root
+hadd VBF_2023_tuples.root ${EOS_PATH}VBF_2023_tuples.root
+hadd ttH_2023_tuples.root ${EOS_PATH}ttH_2023_tuples.root
+
+##############  2023BPix  ##############
+
+hadd Data_2023BPix_tuples.root ${EOS_PATH}Muon0_2023D_v1_2023BPix_tuples.root ${EOS_PATH}Muon0_2023D_v2_2023BPix_tuples.root ${EOS_PATH}Muon1_2023D_v1_2023BPix_tuples.root ${EOS_PATH}Muon1_2023D_v2_2023BPix_tuples.root
+
+hadd DY_2023BPix_tuples.root ${EOS_PATH}DY50to120_2023BPix_tuples.root ${EOS_PATH}DY120to200_2023BPix_tuples.root
+hadd EWK_2023BPix_tuples.root ${EOS_PATH}EWK_2L2J_2023BPix_tuples.root
+hadd TT_2023BPix_tuples.root ${EOS_PATH}TTto2L2Nu_2023BPix_tuples.root ${EOS_PATH}TTtoLNu2Q_2023BPix_tuples.root
+hadd DiBoson_2023BPix_tuples.root ${EOS_PATH}WZto2L2Q_2023BPix_tuples.root ${EOS_PATH}WZto3LNu_2023BPix_tuples.root ${EOS_PATH}ZZto2L2Nu_2023BPix_tuples.root ${EOS_PATH}ZZto2L2Q_2023BPix_tuples.root ${EOS_PATH}ZZto4L_2023BPix_tuples.root ${EOS_PATH}WWto2L2Nu_2023BPix_tuples.root
+hadd ggH_2023BPix_tuples.root ${EOS_PATH}ggH_2023BPix_tuples.root
+hadd VBF_2023BPix_tuples.root ${EOS_PATH}VBF_2023BPix_tuples.root
+hadd ttH_2023BPix_tuples.root ${EOS_PATH}ttH_2023BPix_tuples.root
