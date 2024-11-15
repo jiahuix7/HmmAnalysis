267fd62e6d5626afa8b56de6ab04b317d665660b
Matias Barria <mat.barrial@gmail.com>
2024-11-15 16:14:43 -0600

    add ggh to the new format

--- root_io/tuples/hadd_tuples.sh
+++ root_io/tuples/hadd_tuples.sh
@@ -21,25 +21,39 @@
 
 #hadd EWK_2022_tuples.root EWK_2L2J_2022_tuples.root 
 
-
-
 ## 2023
-hadd Data_2023_tuples.root Muon0_2023C_v1_2022_tuples.root Muon0_2023C_v2_2022_tuples.root Muon0_2023C_v3_2022_tuples.root Muon0_2023C_v4_2022_tuples.root Muon1_2023C_v1_2022_tuples.root Muon1_2023C_v2_2022_tuples.root Muon1_2023C_v3_2022_tuples.root Muon1_2023C_v4_2022_tuples.root
+#hadd Data_2023_tuples.root Muon0_2023C_v1_2022_tuples.root Muon0_2023C_v2_2022_tuples.root Muon0_2023C_v3_2022_tuples.root Muon0_2023C_v4_2022_tuples.root Muon1_2023C_v1_2022_tuples.root Muon1_2023C_v2_2022_tuples.root Muon1_2023C_v3_2022_tuples.root Muon1_2023C_v4_2022_tuples.root
 
-hadd DiBoson_2023_tuples.root WWto2L2Nu_2023_tuples.root WZto2L2Q_2023_tuples.root WZto3LNu_2023_tuples.root ZZto2L2Q_2023_tuples.root ZZto2L2Nu_2023_tuples.root ZZto4L_2023_tuples.root 
+#hadd DiBoson_2023_tuples.root WWto2L2Nu_2023_tuples.root WZto2L2Q_2023_tuples.root WZto3LNu_2023_tuples.root ZZto2L2Q_2023_tuples.root ZZto2L2Nu_2023_tuples.root ZZto4L_2023_tuples.root 
 
-hadd TT_2023_tuples.root TTto2L2Nu_2023_tuples.root TTtoLNu2Q_2023_tuples.root
+#hadd TT_2023_tuples.root TTto2L2Nu_2023_tuples.root TTtoLNu2Q_2023_tuples.root
 
-hadd DY_2023_tuples.root DY50to120_2023_tuples.root DY120to200_2023_tuples.root
+#hadd DY_2023_tuples.root DY50to120_2023_tuples.root DY120to200_2023_tuples.root
 
-hadd EWK_2023_tuples.root EWK_2L2J_2023_tuples.root 
+#hadd EWK_2023_tuples.root EWK_2L2J_2023_tuples.root 
 
-## 2023BPix
-hadd Data_2023BPix_tuples.root Muon0_2023D_v1_2022_tuples.root Muon0_2023D_v2_2022_tuples.root Muon1_2023D_v1_2022_tuples.root Muon1_2023D_v2_2022_tuples.root
+### 2023BPix
+#hadd Data_2023BPix_tuples.root Muon0_2023D_v1_2022_tuples.root Muon0_2023D_v2_2022_tuples.root Muon1_2023D_v1_2022_tuples.root Muon1_2023D_v2_2022_tuples.root
 
-hadd DiBoson_2023BPix_tuples.root WWto2L2Nu_2023BPix_tuples.root WZto2L2Q_2023BPix_tuples.root WZto3LNu_2023BPix_tuples.root ZZto2L2Q_2023BPix_tuples.root ZZto2L2Nu_2023BPix_tuples.root ZZto4L_2023BPix_tuples.root
-hadd TT_2023BPix_tuples.root TTto2L2Nu_2023BPix_tuples.root TTtoLNu2Q_2023BPix_tuples.root
+#hadd DiBoson_2023BPix_tuples.root WWto2L2Nu_2023BPix_tuples.root WZto2L2Q_2023BPix_tuples.root WZto3LNu_2023BPix_tuples.root ZZto2L2Q_2023BPix_tuples.root ZZto2L2Nu_2023BPix_tuples.root ZZto4L_2023BPix_tuples.root
+#hadd TT_2023BPix_tuples.root TTto2L2Nu_2023BPix_tuples.root TTtoLNu2Q_2023BPix_tuples.root
 
-hadd DY_2023BPix_tuples.root DY50to120_2023BPix_tuples.root DY120to200_2023BPix_tuples.root
+#hadd DY_2023BPix_tuples.root DY50to120_2023BPix_tuples.root DY120to200_2023BPix_tuples.root
 
-hadd EWK_2023BPix_tuples.root EWK_2L2J_2023BPix_tuples.root 
+#hadd EWK_2023BPix_tuples.root EWK_2L2J_2023BPix_tuples.root 
+
+
+#Combined
+#hadd Data_Combined_tuples.root Data_2022_tuples.root Data_2022EE_tuples.root Data_2023_tuples.root Data_2023BPix_tuples.root
+
+#hadd DiBoson_Combined_tuples.root DiBoson_2022_tuples.root DiBoson_2022EE_tuples.root DiBoson_2023_tuples.root DiBoson_2023BPix_tuples.root
+
+#hadd TT_Combined_tuples.root TT_2022_tuples.root TT_2022EE_tuples.root TT_2023_tuples.root TT_2023BPix_tuples.root
+
+#hadd DY_Combined_tuples.root DY_2022_tuples.root DY_2022EE_tuples.root DY_2023_tuples.root DY_2023BPix_tuples.root
+
+#hadd EWK_Combined_tuples.root EWK_2022_tuples.root EWK_2022EE_tuples.root EWK_2023_tuples.root EWK_2023BPix_tuples.root
+
+hadd ggH_Combined_tuples.root ggH_2022_tuples.root ggH_2022EE_tuples.root ggH_2023_tuples.root ggH_2023BPix_tuples.root
+hadd VBF_Combined_tuples.root VBF_2022_tuples.root VBF_2022EE_tuples.root VBF_2023_tuples.root VBF_2023BPix_tuples.root
+hadd ttH_Combined_tuples.root ttH_2022_tuples.root ttH_2022EE_tuples.root ttH_2023_tuples.root ttH_2023BPix_tuples.root
