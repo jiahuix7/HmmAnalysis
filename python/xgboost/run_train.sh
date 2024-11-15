aa18c321702361232f2682774e40f7096e0fc202
Claudio San Martin <claudio.sanmartinval@gmail.com>
2024-11-15 11:14:40 -0600

    Include argument to run training script with others HiggsChannels

--- python/xgboost/run_train.sh
+++ python/xgboost/run_train.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Use Channel Under Study as argument only!"
+    exit
+fi
+HIGGS_CHANNEL=$1
 
 ## declare an array variable
 declare -a bkg_subset=("Full" "NoDY50")
 # declare -a sig_subset=("Full" "NottH" "VBF")
-declare -a sig_subset=("NottH" "VBF")
+declare -a sig_subset=("NottH" ${HIGGS_CHANNEL})
 # declare -a eras=("2022" "2022EE" "2022Combined" "2023" "2023BPix" "2023Combined" "Combined")
 declare -a eras=("2022" "2022Combined" "2023Combined" "Combined")
 
@@ -11,11 +18,11 @@ for bs in "${bkg_subset[@]}"; do
     for ss in "${sig_subset[@]}"; do
         echo "--------- B ${bs} - S ${ss} ---------"
         for era in "${eras[@]}"; do
-            python3 train.py VBF ${era} ${bs} ${ss}
+            python3 train.py ${HIGGS_CHANNEL} ${era} ${bs} ${ss}
 
             for type in "signal" "background"; do
                 echo "         --- ${era} ${type} ---"
-                python3 append_xgboost_discriminator_to_tree.py VBF ${era} ${type} ${bs} ${ss}
+                python3 append_xgboost_discriminator_to_tree.py ${HIGGS_CHANNEL} ${era} ${type} ${bs} ${ss}
             done
         done
     done
